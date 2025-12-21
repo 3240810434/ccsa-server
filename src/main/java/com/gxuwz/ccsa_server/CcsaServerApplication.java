ed00ac10e62a8ca9762202e08bf08315475e3651
3240810434 <3240810434@qq.com>
2025-12-21 15:20:10 +0800

    1

--- src/main/java/com/gxuwz/ccsa_server/CcsaServerApplication.java
+++ src/main/java/com/gxuwz/ccsa_server/CcsaServerApplication.java
@@ -1,4 +1,4 @@
-package com.gxuwz.ccsaserver;
+package com.gxuwz.ccsa_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
