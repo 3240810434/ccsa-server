19077e10bace98678bcfeb6662b966350d3bd270
3240810434 <3240810434@qq.com>
2025-12-21 15:08:43 +0800

    Initial commit

---
+++ src/main/java/com/gxuwz/ccsaserver/CcsaServerApplication.java
@@ -0,0 +1,13 @@
+package com.gxuwz.ccsaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcsaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcsaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gxuwz/ccsaserver/CcsaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxuwz.ccsaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcsaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
